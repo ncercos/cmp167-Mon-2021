a8e63d12db6dbb0f7c686394b3c23cddf4aafe13
ncercos <95238172+ncercos@users.noreply.github.com>
2021-12-01 20:35:19 -0500

    Add files via upload

---
+++ Week 2/src/Chaper2.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ * @author Nicholas Cercos
+ * @version 1.0.0
+ * @created 9.13.21
+ * @description Topics of Chapter 2
+ *
+ */
+
+public class Chaper2 {
+	
+	public static void main(String[] args) {
+		/*
+		 * Variables: Placeholder in memory, with a specified amount of space, and given name
+		 * {Data type} {name of variable} ;
+		 * 
+		 * Data Types:
+		 * - Primitive:
+		 * -- Numerical
+		 * --- Integers
+		 * ----- byte: 8 bits -(2^7) to (2^7)-1
+		 * ----- short: 16 bits
+		 * ----- int: 32 bits
+		 * ----- long: 64 bits
+		 * --- Floating Point Numbers
+		 * ----- float: 32 bits
+		 * ----- double: 64 bits
+		 * -- char: 16 bits
+		 * -- boolean: true/false
+		 * - Referential
+		 * -- String
+		 * -- Scanner
+		 * -- BufferReader
+		 */
+		
+		byte numberB;
+		short numberS;
+		int numberI;
+		long numberL;
+		float numberF;
+		double numberD;
+		
+		numberB = 127;
+		numberI = 8; 
+		int n = numberB + numberI;
+		
+		System.out.println(n);
+	}
+
+}
